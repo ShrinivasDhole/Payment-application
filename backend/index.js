35a3994e372c9efa494ecf3e616e5e40095933fe
ShrinivasDhole <shrinivasdhole7@gmail.com>
2024-03-05 21:08:57 +0530

    for vercel

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,11 @@
 const express = require('express');
 const cors = require("cors");
 const rootRouter = require("./routes/index");
-
+const path = require("path");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
-
 app.listen(3000);
